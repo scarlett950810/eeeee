7e0dc2f87136f30872d7bc473c84b6b9dd31feda
Li Hao <Howe0819@gmail.com>
2015-08-27 11:05:13 +0800

    log in version 3

--- MerlionAirlinesSystem/MerlionAirlinesSystem-ejb/src/java/imas/common/entity/InternalMessageEntity.java
+++ MerlionAirlinesSystem/MerlionAirlinesSystem-ejb/src/java/imas/common/entity/InternalMessageEntity.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package irms.common.entity;
+package imas.common.entity;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
