664e58689c3d8e36624a9f2c4edb78823b430c0a
Lei <Lei@YL>
2015-08-28 15:38:47 +0800

    YL01

--- MerlionAirlinesSystem/MerlionAirlinesSystem-war/src/java/imas/web/managedbean/common/LoginManagedBean.java
+++ MerlionAirlinesSystem/MerlionAirlinesSystem-war/src/java/imas/web/managedbean/common/LoginManagedBean.java
@@ -7,6 +7,8 @@ package imas.web.managedbean.common;
 
 import imas.common.sessionbean.LoginSessionBeanLocal;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.ejb.EJB;
@@ -20,15 +22,16 @@ import org.primefaces.context.RequestContext;
  *
  * @author Howard
  */
-@Named(value="loginManagedBean")
+@Named(value = "loginManagedBean")
 @ManagedBean
 @RequestScoped
 public class LoginManagedBean {
+
     @EJB
     private LoginSessionBeanLocal loginSessionBean;
-    
+
     private boolean loggedIn;
- 
+
     /**
      * Creates a new instance of LoginManagedBean
      */
@@ -51,17 +54,46 @@ public class LoginManagedBean {
     public void setLoggedIn(boolean loggedIn) {
         this.loggedIn = loggedIn;
     }
-    
+
     public void doLogin(String staffNo, String password) throws IOException {
         FacesContext fc = FacesContext.getCurrentInstance();
         ExternalContext ec = fc.getExternalContext();
-        
-        if(loginSessionBean.doLogin(staffNo, password)==true){
+
+        if (loginSessionBean.doLogin(staffNo, password) == true) {
             ec.redirect("http://localhost:8080/MerlionAirlinesSystem-war/common/LandingPage.xhtml");
             loggedIn = true;
-        }else{
+        } else {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Loggin Error", "Invalid credentials"));
         }
     }
 
+   public void welcome() {
+        FacesContext.getCurrentInstance().addMessage(
+                null,
+                new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome Back",
+                        "Continue your works."));
+
+    }
+
+    public void logout() {
+        FacesContext.getCurrentInstance().addMessage(
+                null,
+                new FacesMessage(FacesMessage.SEVERITY_WARN,
+                        "You Have Logged Out!",
+                        "Thank you for using Island Furniture Internal Portal!"));
+
+        // invalidate session, and redirect to other pages
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().clear();
+        try {
+            String url = FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath();
+            FacesContext.getCurrentInstance().getExternalContext().redirect(url + "/common/common_login.xhtml");
+
+        } catch (IOException ex) {
+            Logger.getLogger(LoginManagedBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
+
+     
+   
