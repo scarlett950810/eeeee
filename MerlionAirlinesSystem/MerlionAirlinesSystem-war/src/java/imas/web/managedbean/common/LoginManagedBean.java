832c31915ae19941b2ffe23c900af55d68f07527
ruicai <ruicai@r-7-101-25-172.comp.nus.edu.sg>
2015-08-30 11:04:55 +0800

    23

--- MerlionAirlinesSystem/MerlionAirlinesSystem-war/src/java/imas/web/managedbean/common/LoginManagedBean.java
+++ MerlionAirlinesSystem/MerlionAirlinesSystem-war/src/java/imas/web/managedbean/common/LoginManagedBean.java
@@ -7,6 +7,8 @@ package imas.web.managedbean.common;
 
 import imas.common.sessionbean.LoginSessionBeanLocal;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.ejb.EJB;
@@ -65,76 +67,6 @@ public class LoginManagedBean {
         }
     }
 
-}
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package imas.web.managedbean.common;
-
-import imas.common.sessionbean.LoginSessionBeanLocal;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-import javax.ejb.EJB;
-import javax.faces.application.FacesMessage;
-import javax.faces.context.ExternalContext;
-import javax.faces.context.FacesContext;
-import javax.inject.Named;
-import org.primefaces.context.RequestContext;
-
-/**
- *
- * @author Howard
- */
-@Named(value = "loginManagedBean")
-@ManagedBean
-@RequestScoped
-public class LoginManagedBean {
-
-    @EJB
-    private LoginSessionBeanLocal loginSessionBean;
-
-    private boolean loggedIn;
-
-    /**
-     * Creates a new instance of LoginManagedBean
-     */
-    public LoginManagedBean() {
-        this.loggedIn = false;
-    }
-
-    public LoginSessionBeanLocal getLoginSessionBean() {
-        return loginSessionBean;
-    }
-
-    public void setLoginSessionBean(LoginSessionBeanLocal loginSessionBean) {
-        this.loginSessionBean = loginSessionBean;
-    }
-
-    public boolean isLoggedIn() {
-        return loggedIn;
-    }
-
-    public void setLoggedIn(boolean loggedIn) {
-        this.loggedIn = loggedIn;
-    }
-
-    public void doLogin(String staffNo, String password) throws IOException {
-        FacesContext fc = FacesContext.getCurrentInstance();
-        ExternalContext ec = fc.getExternalContext();
-
-        if (loginSessionBean.doLogin(staffNo, password) == true) {
-            ec.redirect(ec.getRequestContextPath() + "/templates/DefaultTemplate.xhtml");
-            loggedIn = true;
-        } else {
-            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Loggin Error", "Invalid credentials"));
-        }
-    }
-
     public void welcome() {
         FacesContext.getCurrentInstance().addMessage(
                 null,
@@ -162,3 +94,73 @@ public class LoginManagedBean {
         }
     }
 }
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package imas.web.managedbean.common;
+//
+//import imas.common.sessionbean.LoginSessionBeanLocal;
+//import java.io.IOException;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
+//import javax.faces.bean.ManagedBean;
+//import javax.faces.bean.RequestScoped;
+//import javax.ejb.EJB;
+//import javax.faces.application.FacesMessage;
+//import javax.faces.context.ExternalContext;
+//import javax.faces.context.FacesContext;
+//import javax.inject.Named;
+//import org.primefaces.context.RequestContext;
+//
+///**
+// *
+// * @author Howard
+// */
+//@Named(value = "loginManagedBean")
+//@ManagedBean
+//@RequestScoped
+//public class LoginManagedBean {
+//
+//    @EJB
+//    private LoginSessionBeanLocal loginSessionBean;
+//
+//    private boolean loggedIn;
+//
+//    /**
+//     * Creates a new instance of LoginManagedBean
+//     */
+//    public LoginManagedBean() {
+//        this.loggedIn = false;
+//    }
+//
+//    public LoginSessionBeanLocal getLoginSessionBean() {
+//        return loginSessionBean;
+//    }
+//
+//    public void setLoginSessionBean(LoginSessionBeanLocal loginSessionBean) {
+//        this.loginSessionBean = loginSessionBean;
+//    }
+//
+//    public boolean isLoggedIn() {
+//        return loggedIn;
+//    }
+//
+//    public void setLoggedIn(boolean loggedIn) {
+//        this.loggedIn = loggedIn;
+//    }
+//
+//    public void doLogin(String staffNo, String password) throws IOException {
+//        FacesContext fc = FacesContext.getCurrentInstance();
+//        ExternalContext ec = fc.getExternalContext();
+//
+//        if (loginSessionBean.doLogin(staffNo, password) == true) {
+//            ec.redirect(ec.getRequestContextPath() + "/templates/DefaultTemplate.xhtml");
+//            loggedIn = true;
+//        } else {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Loggin Error", "Invalid credentials"));
+//        }
+//    }
+
+    
