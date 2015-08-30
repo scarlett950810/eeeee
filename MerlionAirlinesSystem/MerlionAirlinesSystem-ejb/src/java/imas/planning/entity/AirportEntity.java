596c9d3891fa80678e5c1a66f418bcf343caa947
Li Hao <Howe0819@gmail.com>
2015-08-30 20:42:07 +0800

    reconfigure settings

--- MerlionAirlinesSystem/MerlionAirlinesSystem-ejb/src/java/imas/planning/entity/AirportEntity.java
+++ MerlionAirlinesSystem/MerlionAirlinesSystem-ejb/src/java/imas/planning/entity/AirportEntity.java
@@ -6,10 +6,14 @@
 package imas.planning.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 /**
  *
@@ -27,6 +31,7 @@ public class AirportEntity implements Serializable {
     private String cityName;
     
     private String airportName;
+    
 
     /**
      * Get the value of airportName
--- MerlionAirlinesSystem/MerlionAirlinesSystem-war/src/java/imas/web/managedbean/common/LoginManagedBean.java
+++ MerlionAirlinesSystem/MerlionAirlinesSystem-war/src/java/imas/web/managedbean/common/LoginManagedBean.java
@@ -16,21 +16,21 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import org.primefaces.context.RequestContext;
 
 /**
  *
  * @author Howard
  */
-@Named(value="loginManagedBean")
+@Named(value = "loginManagedBean")
 @ManagedBean
 @RequestScoped
 public class LoginManagedBean {
+
     @EJB
     private LoginSessionBeanLocal loginSessionBean;
-    
+
     private boolean loggedIn;
- 
+
     /**
      * Creates a new instance of LoginManagedBean
      */
@@ -53,25 +53,32 @@ public class LoginManagedBean {
     public void setLoggedIn(boolean loggedIn) {
         this.loggedIn = loggedIn;
     }
-    
+
     public void doLogin(String staffNo, String password) throws IOException {
         FacesContext fc = FacesContext.getCurrentInstance();
         ExternalContext ec = fc.getExternalContext();
-        
-        if(loginSessionBean.doLogin(staffNo, password)==true){
-              ec.redirect("common/common_landing.xhtml"); //
+
+        if (loginSessionBean.doLogin(staffNo, password) == true) {
+//            ec.redirect("common/LandingPage.xhtml");
+            ec.redirect("common/common_landing.xhtml");
 //            ec.redirect(ec.getRequestContextPath() + "/templates/DefaultTemplate.xhtml");
             loggedIn = true;
-        }else{
+        } else {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Loggin Error", "Invalid credentials"));
         }
     }
 
-    public void welcome() {
-        FacesContext.getCurrentInstance().addMessage(
-                null,
-                new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome Back",
-                        "Continue your works."));
+    public void welcome() //        FacesContext.getCurrentInstance().addMessage(
+    //                null,
+    //                new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome Back",
+    //                        "Continue your works."));
+    {
+        addMessage("Welcome Back", "Please Continue your work.");
+    }
+
+    public void addMessage(String summary, String detail) {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, summary, detail);
+        FacesContext.getCurrentInstance().addMessage(null, message);
 
     }
 
@@ -80,9 +87,8 @@ public class LoginManagedBean {
                 null,
                 new FacesMessage(FacesMessage.SEVERITY_WARN,
                         "You Have Logged Out!",
-                        "Thank you for using Island Furniture Internal Portal!"));
-
-        // invalidate session, and redirect to other pages
+                        "Thank you for using Merlion Airlines System!"));
+//       
         FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
         FacesContext.getCurrentInstance().getExternalContext().getSessionMap().clear();
         try {
@@ -92,6 +98,7 @@ public class LoginManagedBean {
         } catch (IOException ex) {
             Logger.getLogger(LoginManagedBean.class.getName()).log(Level.SEVERE, null, ex);
         }
+
     }
 }
 ///*
