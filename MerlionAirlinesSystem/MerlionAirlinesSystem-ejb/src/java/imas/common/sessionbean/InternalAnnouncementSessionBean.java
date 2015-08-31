dd517bd1296a1c6e2b474ef6405e6cc285659eb9
scarlett950810 <scarlett.dongyan@gmail.com>
2015-08-31 11:52:33 +0800

    separate internalAnnouncement into new sessionbeans and managedbeans & use session variable for storing global variable.

---
+++ MerlionAirlinesSystem/MerlionAirlinesSystem-ejb/src/java/imas/common/sessionbean/InternalAnnouncementSessionBean.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imas.common.sessionbean;
+
+import imas.common.entity.InternalAnnouncementEntity;
+import imas.common.entity.StaffEntity;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateful;
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Scarlett
+ */
+@Stateful
+public class InternalAnnouncementSessionBean implements InternalAnnouncementSessionBeanLocal {
+
+    
+    
+    @PersistenceContext
+    private EntityManager entityManager;
+    
+    
+    @Override
+    public List<InternalAnnouncementEntity> getAllAnnouncements() {
+        StaffEntity staffEntity = (StaffEntity) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("staffEntity");
+        Query queryForAllAnnoucements = entityManager.createQuery("SELECT a FROM InternalAnnouncementEntity a WHERE a.receiver = :staffEntity");
+        queryForAllAnnoucements.setParameter("staffEntity", (StaffEntity) staffEntity);
+        
+        System.out.print("internalAnnouncementSessionBean.getAllAnnoucements called");
+        System.out.print(queryForAllAnnoucements.getResultList());
+        List<InternalAnnouncementEntity> announcements = new ArrayList();
+        queryForAllAnnoucements.getResultList().stream().forEach((o) -> {
+            InternalAnnouncementEntity a = (InternalAnnouncementEntity) o;
+            announcements.add(a);
+            System.out.print(a);
+        });
+
+        return (List<InternalAnnouncementEntity>) announcements;
+    }
+    
+    
+}
--- MerlionAirlinesSystem/MerlionAirlinesSystem-war/src/java/imas/web/managedbean/common/LoginManagedBean.java
+++ MerlionAirlinesSystem/MerlionAirlinesSystem-war/src/java/imas/web/managedbean/common/LoginManagedBean.java
@@ -5,11 +5,8 @@
  */
 package imas.web.managedbean.common;
 
-import imas.common.entity.InternalAnnouncementEntity;
-import imas.common.entity.StaffEntity;
 import imas.common.sessionbean.LoginSessionBeanLocal;
 import java.io.IOException;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
@@ -32,13 +29,6 @@ public class LoginManagedBean {
     @EJB
     private LoginSessionBeanLocal loginSessionBean;
 
-    private StaffEntity staffEntity;
-    
-    private List<InternalAnnouncementEntity> allAnnouncements;
-    
-    private List<InternalAnnouncementEntity> unreadAnnouncements;
-    
-
     /**
      * Creates a new instance of LoginManagedBean
      */
@@ -53,25 +43,16 @@ public class LoginManagedBean {
         this.loginSessionBean = loginSessionBean;
     }
 
-    public StaffEntity getStaffEntity() {
-        return staffEntity;
-    }
-
-    public void setStaffEntity(StaffEntity staffEntity) {
-        this.staffEntity = staffEntity;
-    }
-
     public void doLogin(String staffNo, String password) throws IOException {
         FacesContext fc = FacesContext.getCurrentInstance();
         ExternalContext ec = fc.getExternalContext();
-        StaffEntity staffEntity_result = loginSessionBean.doLogin(staffNo, password);
         
-        if (staffEntity_result != null) {
-            ec.redirect(ec.getRequestContextPath() + "/templates/DefaultTemplate.xhtml");
-            this.setStaffEntity(staffEntity_result);
+        if (loginSessionBean.doLogin(staffNo, password)) {
+            ec.redirect(ec.getRequestContextPath() + "/common/common_landing.xhtml");
         } else {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Loggin Error", "Invalid credentials"));
         }
+        
     }
 
     public void welcome() {
@@ -87,7 +68,7 @@ public class LoginManagedBean {
                 null,
                 new FacesMessage(FacesMessage.SEVERITY_WARN,
                         "You Have Logged Out!",
-                        "Thank you for using Island Furniture Internal Portal!"));
+                        "Thank you for using Merlion Airline Internal Portal!"));
 
         // invalidate session, and redirect to other pages
         FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
@@ -100,20 +81,5 @@ public class LoginManagedBean {
             Logger.getLogger(LoginManagedBean.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public List<InternalAnnouncementEntity> getAllAnnouncements() {
-        return loginSessionBean.getAllAnnoucements(staffEntity);
-    }
-
-    public void setAllAnnouncements(List<InternalAnnouncementEntity> allAnnouncements) {
-        this.allAnnouncements = allAnnouncements;
-    }
-    
-    public List<InternalAnnouncementEntity> getUnreadAnnouncements() {
-        return loginSessionBean.getUnreadAnnoucements(staffEntity);
-    }
-    
-    public void setUnreadAnnouncements(List<InternalAnnouncementEntity> unreadAnnouncements) {
-        this.unreadAnnouncements = unreadAnnouncements;
-    }
+   
 }
