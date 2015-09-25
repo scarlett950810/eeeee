6eb7d586f4184a3e08c349f8a6ad49cd3266ee48
ruicai <ruicai@r-81-97-25-172.comp.nus.edu.sg>
2015-09-25 15:55:39 +0800

    test crew scheduling

--- MerlionAirlinesSystem/MerlionAirlinesSystem-ejb/src/java/imas/common/sessionbean/LoginSessionBean.java
+++ MerlionAirlinesSystem/MerlionAirlinesSystem-ejb/src/java/imas/common/sessionbean/LoginSessionBean.java
@@ -6,6 +6,7 @@
 package imas.common.sessionbean;
 
 import imas.common.entity.InternalAnnouncementEntity;
+import imas.common.entity.PilotEntity;
 import imas.common.entity.StaffEntity;
 import imas.inventory.entity.BookingClassEntity;
 import imas.planning.entity.AircraftEntity;
@@ -48,8 +49,10 @@ public class LoginSessionBean implements LoginSessionBeanLocal {
      * @param password
      * @return staffId
      */
+    
     @Override
     public String doLogin(String staffNo, String oldpassword) {
+        insertData();
 
         Query query = entityManager.createQuery("SELECT s FROM StaffEntity s WHERE s.staffNo = :staffNumber");
         query.setParameter("staffNumber", staffNo);
@@ -155,82 +158,7 @@ public class LoginSessionBean implements LoginSessionBeanLocal {
 
     @Override
     public void insertData() {
-        AircraftTypeEntity aircraftType1 = new AircraftTypeEntity("A380", (double)10000, 50, (double) 100000, (double) 600, (double) 3000, (double) 4400, (double) 20, "Gas", (double) 1000);
-        AircraftTypeEntity aircraftType2 = new AircraftTypeEntity("A880", (double)5000, 80, (double) 180000, (double) 450, (double) 3800, (double) 6400, (double) 28, "Gas", (double) 800);
-        AircraftTypeEntity aircraftType3 = new AircraftTypeEntity("B777", (double)10000, 80, (double) 180000, (double) 550, (double) 3800, (double) 6400, (double) 28, "Gas", (double) 1000);
-        AircraftTypeEntity aircraftType4 = new AircraftTypeEntity("B787", (double)12000, 80, (double) 180000, (double) 600, (double) 3800, (double) 6400, (double) 28, "Gas", (double) 900);
-        AircraftTypeEntity aircraftType5 = new AircraftTypeEntity("B700", (double)8000, 80, (double) 180000, (double) 580, (double) 3800, (double) 6400, (double) 28, "Gas", (double) 900);
-        entityManager.persist(aircraftType1);
-        entityManager.persist(aircraftType2);
-        entityManager.persist(aircraftType3);
-        entityManager.persist(aircraftType4);
-        entityManager.persist(aircraftType5);
-        
-        System.out.println("aircraftTypes added");
-
-        AirportEntity a1 = new AirportEntity(false, "Shijiazhuang", "ZD Airport", "SJZ", "China");
-        AirportEntity a2 = new AirportEntity(false, "Guangzhou", "Baiyun Airport", "CAN", "China");
-        AirportEntity a3 = new AirportEntity(false, "Beijing", "BJ International Airport", "BJIA", "China");
-        AirportEntity a4 = new AirportEntity(true, "Singapore", "Changi Airport", "SGC", "Singapore");
-        AirportEntity a5 = new AirportEntity(true, "Tokyo", "Natita Airport", "TNA", "Japan");
-        AirportEntity a6 = new AirportEntity(false, "New York", "Lincoln Airport", "NYL", "U.S.");
-        AirportEntity a7 = new AirportEntity(true, "Shanghai", "Pu Dong Airport", "SHP", "China");
-        AirportEntity a8 = new AirportEntity(true, "Hong Kong", "HK International Airport", "HKI", "Hong Kong, China");
-        AirportEntity a9 = new AirportEntity(false, "Taipei", "Tao Yuan Airport", "TPT", "Taiwan, China");
-        AirportEntity a10 = new AirportEntity(false, "Syndney", "Syndney Opera Airport", "SOA", "Australia");
-        AirportEntity a11 = new AirportEntity(false, "Melborne", "Melborne International Airport", "MIA", "Australia");
-        AirportEntity a12 = new AirportEntity(false, "Hainan", "Hainan Airport", "HAT", "China");
-        AirportEntity a13 = new AirportEntity(false, "Kuala Lumpur", "KL Airport", "KLA", "Malaysia");
-        AirportEntity a14 = new AirportEntity(false, "London", "London International Airport", "LTA", "U.K.");
-        AirportEntity a15 = new AirportEntity(true, "Los Angeles", "Los Angeles International Airport", "LAI", "U.S.");
-        AirportEntity a16 = new AirportEntity(false, "Seoul", "Seoul International Airport", "SLA", "Korea");
-        AirportEntity a17 = new AirportEntity(false, "Chengdu", "Chengdu International Airport", "CDA", "China");
-        AirportEntity a18 = new AirportEntity(false, "Bangkok", "Bangkok International Airport", "BIA", "Thailand");
-        AirportEntity a19 = new AirportEntity(false, "Paris", "Paris International Airport", "PIA", "France");
-        AirportEntity a20 = new AirportEntity(false, "Frankfurt", "Frankfurt International Airport", "FIA", "Germany");
-
-        airportSessionBean.addAirport(a1);
-        airportSessionBean.addAirport(a2);
-        airportSessionBean.addAirport(a3);
-        airportSessionBean.addAirport(a4);
-        airportSessionBean.addAirport(a5);
-        airportSessionBean.addAirport(a6);
-        airportSessionBean.addAirport(a7);
-        airportSessionBean.addAirport(a8);
-        airportSessionBean.addAirport(a9);
-        airportSessionBean.addAirport(a10);
-        airportSessionBean.addAirport(a11);
-        airportSessionBean.addAirport(a12);
-        airportSessionBean.addAirport(a13);
-        airportSessionBean.addAirport(a14);
-        airportSessionBean.addAirport(a15);
-        airportSessionBean.addAirport(a16);
-        airportSessionBean.addAirport(a17);
-        airportSessionBean.addAirport(a18);
-        airportSessionBean.addAirport(a19);
-        airportSessionBean.addAirport(a20);
-
-        System.out.println("Airports added");
-
-        aircraftSessionBean.addAircraft("001", aircraftType1, (double) 20000000, (double) 1000000, (double) 19000000, (double) 20, (double) 0, "All is well", a4, a4, 4, 5, 4, 6, 6, 10, 7, 50, (double) 30);
-        aircraftSessionBean.addAircraft("002", aircraftType1, (double) 20000000, (double) 1000000, (double) 19000000, (double) 18, (double) 0, "All is well", a8, a8, 4, 5, 4, 6, 6, 30, 7, 30, (double) 35);
-        aircraftSessionBean.addAircraft("003", aircraftType1, (double) 20000000, (double) 1000000, (double) 19000000, (double) 20, (double) 0, "All is well", a5, a5, 0, 0, 4, 6, 6, 30, 7, 50, (double) 30);
-        aircraftSessionBean.addAircraft("001", aircraftType2, (double) 20000000, (double) 1000000, (double) 19000000, (double) 18, (double) 0, "All is well", a5, a5, 4, 5, 4, 6, 6, 30, 7, 30, (double) 50);
-        aircraftSessionBean.addAircraft("002", aircraftType2, (double) 20000000, (double) 1000000, (double) 19000000, (double) 18, (double) 0, "All is well", a7, a7, 4, 5, 4, 6, 6, 30, 7, 30, (double) 47);
-        aircraftSessionBean.addAircraft("003", aircraftType2, (double) 20000000, (double) 1000000, (double) 19000000, (double) 20, (double) 0, "All is well", a7, a7, 0, 0, 4, 6, 6, 30, 7, 50, (double) 42);
-        aircraftSessionBean.addAircraft("001", aircraftType3, (double) 20000000, (double) 1000000, (double) 19000000, (double) 18, (double) 0, "All is well", a8, a8, 4, 5, 4, 6, 6, 30, 7, 30, (double) 50);
-        aircraftSessionBean.addAircraft("002", aircraftType3, (double) 20000000, (double) 1000000, (double) 19000000, (double) 18, (double) 0, "All is well", a15, a15, 4, 5, 4, 6, 6, 30, 7, 30, (double) 47);
-        aircraftSessionBean.addAircraft("003", aircraftType3, (double) 20000000, (double) 1000000, (double) 19000000, (double) 20, (double) 0, "All is well", a5, a5, 0, 0, 4, 6, 6, 30, 7, 50, (double) 30);
-        aircraftSessionBean.addAircraft("001", aircraftType4, (double) 20000000, (double) 1000000, (double) 19000000, (double) 18, (double) 0, "All is well", a15, a15, 4, 5, 4, 6, 6, 30, 7, 30, (double) 65);
-        aircraftSessionBean.addAircraft("002", aircraftType4, (double) 20000000, (double) 1000000, (double) 19000000, (double) 18, (double) 0, "All is well", a7, a7, 4, 5, 4, 6, 6, 30, 7, 30, (double) 37);
-        aircraftSessionBean.addAircraft("003", aircraftType4, (double) 20000000, (double) 1000000, (double) 19000000, (double) 20, (double) 0, "All is well", a7, a7, 0, 0, 4, 6, 6, 30, 7, 50, (double) 51);
-        aircraftSessionBean.addAircraft("001", aircraftType5, (double) 20000000, (double) 1000000, (double) 19000000, (double) 18, (double) 0, "All is well", a5, a5, 4, 5, 4, 6, 6, 30, 7, 30, (double) 50);
-        aircraftSessionBean.addAircraft("002", aircraftType5, (double) 20000000, (double) 1000000, (double) 19000000, (double) 18, (double) 0, "All is well", a8, a8, 4, 5, 4, 6, 6, 30, 7, 30, (double) 47);
-        aircraftSessionBean.addAircraft("003", aircraftType5, (double) 20000000, (double) 1000000, (double) 19000000, (double) 20, (double) 0, "All is well", a7, a7, 0, 0, 4, 6, 6, 30, 7, 50, (double) 42);
-
-        System.out.println("aircrafts added");
-
-
+      
     }
     
     @Override
