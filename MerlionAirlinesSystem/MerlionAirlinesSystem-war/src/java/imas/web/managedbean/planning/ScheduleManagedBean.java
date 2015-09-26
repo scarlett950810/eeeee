d6eee8eb387b5960f9a58957a6685475dc2a9a21
Cai Rui <cairui0112@gmail.com>
2015-09-26 14:26:10 +0800

    Merge origin/master
    Conflicts:
    	MerlionAirlinesSystem/MerlionAirlinesSystem-war/src/java/imas/web/managedbean/common/LoginManagedBean.java

--- MerlionAirlinesSystem/MerlionAirlinesSystem-war/src/java/imas/web/managedbean/planning/ScheduleManagedBean.java
+++ MerlionAirlinesSystem/MerlionAirlinesSystem-war/src/java/imas/web/managedbean/planning/ScheduleManagedBean.java
@@ -145,6 +145,7 @@ public class ScheduleManagedBean implements Serializable {
                 //set the departure time of flight in the flights 
                 Date departureT = combineTwoDate(f.getDepartureDate(), departureDateTemp);
                 f1.setDepartureDate(departureT);
+                f1.setFlightNo(f.getFlightNo());
                 cal.setTime(departureT);
                 cal.add(Calendar.MINUTE, (int) (routeSelected.getFlightHours() * 60 + 0.5d));
                 
@@ -154,6 +155,7 @@ public class ScheduleManagedBean implements Serializable {
                 f1.setRoute(f.getRoute());
                 departureT = combineTwoDate(f.getReverseFlight().getDepartureDate(), departureDateTemp);
                 f1.getReverseFlight().setDepartureDate(departureT);
+                f1.setFlightNo(f.getReverseFlight().getFlightNo());
                 cal.setTime(departureT);
                 cal.add(Calendar.MINUTE, (int) (routeSelected.getFlightHours() * 60 + 0.5d));
                 
