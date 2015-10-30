62dd2c907ce3f932bf75361c38437bba77a5ca35
Li Hao <Howe0819@gmail.com>
2015-10-30 21:42:59 +0800

    after second system release

--- MerlionAirlinesSystem/MerlionAirlinesExternalSystem/src/java/emas/web/managedbean/distribution/CustomerBookTicketManagedBean.java
+++ MerlionAirlinesSystem/MerlionAirlinesExternalSystem/src/java/emas/web/managedbean/distribution/CustomerBookTicketManagedBean.java
@@ -410,6 +410,7 @@ public class CustomerBookTicketManagedBean implements Serializable {
         }
     
     public void afterPay() throws IOException{
+        System.out.print("after pay called");
         referenceNumber = makeBookingSessionBean.generateItinerary(flights, passengers, title, firstName, lastName, address, city, country, email, contactNumber, postCode, "paid", totalPrice);
 //        FacesContext.getCurrentInstance().getExternalContext().redirect("../ReportController?referenceNumber=" + referenceNumber);
         RequestContext.getCurrentInstance().execute("window.open(\"https://localhost:8181/MerlionAirlinesExternalSystem/ReportController?referenceNumber=" + referenceNumber+ "&passportNumber=" + passengers.get(0).getPassportNumber() + "&passengerName=" + passengers.get(0).getTitle() + " " + passengers.get(0).getFirstName()+" "+passengers.get(0).getLastName()+"\")");
