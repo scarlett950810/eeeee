9d8d468accadb7c3ef67b728059c1a9eeab632e6
Li Hao <Howe0819@gmail.com>
2015-11-12 02:37:04 +0800

    GDS Make Booking Session Bean

--- MerlionAirlinesSystem/MerlionAirlinesSystem-ejb/src/java/imas/utility/sessionbean/EmailManager.java
+++ MerlionAirlinesSystem/MerlionAirlinesSystem-ejb/src/java/imas/utility/sessionbean/EmailManager.java
@@ -317,4 +317,75 @@ public class EmailManager {
             System.out.print(e);
         }
     }//run
+    
+    public static boolean runGDSBookingConfirmation(String addressedTo, String subject, String flight, String passenger, String referenceNumber) {
+
+        //String toAddress = "winexpress.noreply@gmail.com"; // set the email address to send to
+        String recipients = addressedTo.trim(); // trim the recipients address to avoid space
+        //String contentType = "text/plain"; // set content type -- only plain text
+        String fromAddress = "lihao0426@gmail.com";
+        String smtpHost = "smtp.gmail.com"; // set the mailbox to send from 
+        int smtpPort = 587; // set port
+        String username = "lihao0426@gmail.com"; // mailbox address
+        String password = "howe0819!"; // *password*
+
+        try {
+            //Address recipient = new InternetAddress(addressedTo.trim());
+            //Address[] recipients = {recipient};
+            Properties props = System.getProperties(); //
+            props.put("mail.smtp.starttls.enable", "true");
+//            Session session = Session.getDefaultInstance(props);
+            Session session = Session.getInstance(props, new GMailAuthenticator(username, password));
+
+            MimeMessage message = new MimeMessage(session); // set message type
+
+            //message.setFrom(new InternetAddress(toAddress));
+            message.setRecipients(Message.RecipientType.TO, recipients);
+            message.setFrom(new InternetAddress(fromAddress));
+            //message.setRecipients(Message.RecipientType.TO, toAddress);
+            //message.setReplyTo(InternetAddress.parse(recipients, false));
+            message.setSubject(subject);
+
+            // This mail has 2 part, the BODY and the embedded image
+            MimeMultipart multipart = new MimeMultipart("related");
+
+            // first part (the html)
+            BodyPart messageBodyPart = new MimeBodyPart();
+            String htmlText = "<body>"
+                    + "<img src=\"http://i.imgsafe.org/260c51a.png\" width=\"200px\">"
+                    + "<h3>Booking Confirmation</h3><br><br><br>"
+                    + "<h2>Reference Number: " + referenceNumber
+                    + "<br><br>" + flight
+                    + "<h3>Passenger List</h3>"
+                    + "<table>"
+                    + "  <tr>"
+                    + "    <th>Name</th>"
+                    + "    <th>Passport Number</th>"
+                    + "    <th>Nationality</th>"
+                    + "  </tr>";
+            htmlText = htmlText + passenger + "</table>";
+            htmlText = htmlText + "</body>";
+            
+            messageBodyPart.setContent(htmlText, "text/html");
+            // add it
+            multipart.addBodyPart(messageBodyPart);
+
+            message.setContent(multipart);
+            message.setSentDate(new Date());
+
+            Transport transport = session.getTransport("smtp"); // set protocol
+            transport.connect(smtpHost, smtpPort, username, password); // connect mail server
+            transport.sendMessage(message, message.getAllRecipients()); // send email
+            transport.close(); // close connection
+
+            return true;
+        } catch (MessagingException messagingException) {
+            System.out.print(messagingException);
+            return false;
+
+        } catch (Exception e) {
+            System.out.print(e);
+            return false;
+        }
+    }//run
 }
